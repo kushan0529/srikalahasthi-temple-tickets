71e84ab376e78371f34d2a8925ba429f90231f29
kushan <kkushan090@gmail.com>
2026-04-22 22:37:34 +0530

    resolves user model error

--- backend/Models/user-Model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const joi = require('joi')
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-})
-module.exports=mongoose.model('User',userSchema)
